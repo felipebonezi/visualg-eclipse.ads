5c3132e4d5dafc085da871ba964100ecc6c75ad1
Victor <victor.vilmarques@gmail.com>
2014-07-05 13:12:34 -0300

    Revert "Teste"
    This reverts commit 9a35ce1406c15baff9ef4cb74a8fdc878bbaf795.

--- src/visualgeclipse/ads/editors/ColorManager.java
+++ src/visualgeclipse/ads/editors/ColorManager.java
@@ -12,8 +12,6 @@ public class ColorManager {
 
 	protected Map fColorTable = new HashMap(10);
 
-	private String teste;
-	
 	public void dispose() {
 		Iterator e = fColorTable.values().iterator();
 		while (e.hasNext())
